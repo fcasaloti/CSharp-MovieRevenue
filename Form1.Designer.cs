ad55b02a165285df1e27e150b52c05e730f831fd
fcasaloti <fcasaloti@hotmail.com>
2020-05-26 20:10:50 -0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace CSIS1175_003F2019FernandoCasalotiSilvaAS4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sortNameBtn = new System.Windows.Forms.Button();
+            this.sortRevenueBtn = new System.Windows.Forms.Button();
+            this.resultsLb = new System.Windows.Forms.ListBox();
+            this.searchGb = new System.Windows.Forms.GroupBox();
+            this.revenueRbtn = new System.Windows.Forms.RadioButton();
+            this.nameRbtn = new System.Windows.Forms.RadioButton();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.searchTb = new System.Windows.Forms.TextBox();
+            this.searchGb.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sortNameBtn
+            // 
+            this.sortNameBtn.Location = new System.Drawing.Point(39, 44);
+            this.sortNameBtn.Name = "sortNameBtn";
+            this.sortNameBtn.Size = new System.Drawing.Size(114, 73);
+            this.sortNameBtn.TabIndex = 0;
+            this.sortNameBtn.Text = "Sort by Name";
+            this.sortNameBtn.UseVisualStyleBackColor = true;
+            this.sortNameBtn.Click += new System.EventHandler(this.sortNameBtn_Click);
+            // 
+            // sortRevenueBtn
+            // 
+            this.sortRevenueBtn.Location = new System.Drawing.Point(39, 136);
+            this.sortRevenueBtn.Name = "sortRevenueBtn";
+            this.sortRevenueBtn.Size = new System.Drawing.Size(114, 72);
+            this.sortRevenueBtn.TabIndex = 1;
+            this.sortRevenueBtn.Text = "Sort by Revenue";
+            this.sortRevenueBtn.UseVisualStyleBackColor = true;
+            this.sortRevenueBtn.Click += new System.EventHandler(this.sortRevenueBtn_Click);
+            // 
+            // resultsLb
+            // 
+            this.resultsLb.FormattingEnabled = true;
+            this.resultsLb.ItemHeight = 20;
+            this.resultsLb.Location = new System.Drawing.Point(172, 44);
+            this.resultsLb.Name = "resultsLb";
+            this.resultsLb.Size = new System.Drawing.Size(541, 164);
+            this.resultsLb.TabIndex = 2;
+            // 
+            // searchGb
+            // 
+            this.searchGb.Controls.Add(this.revenueRbtn);
+            this.searchGb.Controls.Add(this.nameRbtn);
+            this.searchGb.Controls.Add(this.searchBtn);
+            this.searchGb.Controls.Add(this.searchTb);
+            this.searchGb.Location = new System.Drawing.Point(172, 231);
+            this.searchGb.Name = "searchGb";
+            this.searchGb.Size = new System.Drawing.Size(541, 143);
+            this.searchGb.TabIndex = 3;
+            this.searchGb.TabStop = false;
+            this.searchGb.Text = "Search";
+            // 
+            // revenueRbtn
+            // 
+            this.revenueRbtn.AutoSize = true;
+            this.revenueRbtn.Location = new System.Drawing.Point(171, 97);
+            this.revenueRbtn.Name = "revenueRbtn";
+            this.revenueRbtn.Size = new System.Drawing.Size(120, 24);
+            this.revenueRbtn.TabIndex = 3;
+            this.revenueRbtn.TabStop = true;
+            this.revenueRbtn.Text = "By Revenue";
+            this.revenueRbtn.UseVisualStyleBackColor = true;
+            // 
+            // nameRbtn
+            // 
+            this.nameRbtn.AutoSize = true;
+            this.nameRbtn.Location = new System.Drawing.Point(23, 97);
+            this.nameRbtn.Name = "nameRbtn";
+            this.nameRbtn.Size = new System.Drawing.Size(124, 24);
+            this.nameRbtn.TabIndex = 2;
+            this.nameRbtn.TabStop = true;
+            this.nameRbtn.Text = "Within Name";
+            this.nameRbtn.UseVisualStyleBackColor = true;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(327, 34);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(129, 40);
+            this.searchBtn.TabIndex = 1;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // searchTb
+            // 
+            this.searchTb.Location = new System.Drawing.Point(23, 34);
+            this.searchTb.Name = "searchTb";
+            this.searchTb.Size = new System.Drawing.Size(268, 26);
+            this.searchTb.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 409);
+            this.Controls.Add(this.searchGb);
+            this.Controls.Add(this.resultsLb);
+            this.Controls.Add(this.sortRevenueBtn);
+            this.Controls.Add(this.sortNameBtn);
+            this.Name = "MainForm";
+            this.Text = "Fernando Movie Revenue";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.searchGb.ResumeLayout(false);
+            this.searchGb.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sortNameBtn;
+        private System.Windows.Forms.Button sortRevenueBtn;
+        private System.Windows.Forms.ListBox resultsLb;
+        private System.Windows.Forms.GroupBox searchGb;
+        private System.Windows.Forms.TextBox searchTb;
+        private System.Windows.Forms.RadioButton revenueRbtn;
+        private System.Windows.Forms.RadioButton nameRbtn;
+        private System.Windows.Forms.Button searchBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSIS1175_003F2019FernandoCasalotiSilvaAS4
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            nameRbtn.Checked = true;
+        }
+
+        //Sort by name button
+        private void sortNameBtn_Click(object sender, EventArgs e)
+        {
+            //Open Files and create arrays
+            StreamReader inFile;
+            inFile = File.OpenText("MovieRevenue.txt");
+            int arraySize = File.ReadLines("MovieRevenue.txt").Count();
+            inFile.Close();
+            string[] movie = new string[arraySize];
+            double[] revenueSorted = new double[arraySize];
+
+            //Read File and write in arrays
+            ReadIntoArray(movie, revenueSorted);
+
+            //Selection Sort
+            SelectionSortByName(movie, revenueSorted);
+
+            searchTb.Text = "";
+            resultsLb.Items.Clear();
+            for (int idx = 0; idx < movie.Length; idx++)
+            {
+                DisplayRevenue(movie, revenueSorted, idx, " >===> ");
+            }
+        }
+
+        //Sort by Revenue button
+        private void sortRevenueBtn_Click(object sender, EventArgs e)
+        {
+            //Open Files and create arrays
+            StreamReader inFile;
+            inFile = File.OpenText("MovieRevenue.txt");
+            int arraySize = File.ReadLines("MovieRevenue.txt").Count();
+            inFile.Close();
+            string[] movie = new string[arraySize];
+            double[] revenueSorted = new double[arraySize];
+
+            //Read File and write in arrays
+            ReadIntoArray(movie, revenueSorted);
+
+            //Selection Sort
+            SelectionSortByRevenue(movie, revenueSorted);
+
+            searchTb.Text = "";
+            resultsLb.Items.Clear();
+            for (int idx = 0; idx < movie.Length; idx++)
+            {
+                DisplayRevenue(movie, revenueSorted, idx, " <===< ");
+            }
+        }
+
+        //Read file and write on Arrays
+        private void ReadIntoArray(string[] movieArray, double[] revenueArray)
+        {
+
+            //Open Files
+            StreamReader inFile;
+            inFile = File.OpenText("MovieRevenue.txt");
+
+            //Loop to read lines of the file and insert in the arrays
+            int idx = 0;
+            while (!inFile.EndOfStream)
+            {
+                //Read each line, trim empty spaces, separate movie and revenue and insert in the arrays
+                string dataFile = "";
+                dataFile = inFile.ReadLine().Trim();
+                char[] delim = { '{', '}', '@', '=', '#' };
+                string[] divided = dataFile.Split(delim);
+
+                //Insert the content split into different arrays
+                movieArray[idx] = divided[0];
+                revenueArray[idx] = double.Parse(divided[1]);
+
+                //Increase idx
+                idx += 1;
+            }
+
+            //Close File
+            inFile.Close();
+        }
+
+        //Sort by Name
+        private void SelectionSortByName(string[] arr, double[] arr1)
+        {
+            int smallest, idx, idx1;
+            double temp;
+            string temp1;
+            for (idx = 0; idx < arr.Length - 1; idx++)
+            {
+                smallest = idx;
+                for (idx1 = idx + 1; idx1 < arr.Length; idx1++)
+                {
+                    if (string.Compare(arr[idx1], arr[smallest], true) < 0)
+                    {
+                        smallest = idx1;
+                    }
+                }
+                //SelectionSort Movie
+                temp1 = arr[smallest];
+                arr[smallest] = arr[idx];
+                arr[idx] = temp1;
+
+                //Change position Revenue
+                temp = arr1[smallest];
+                arr1[smallest] = arr1[idx];
+                arr1[idx] = temp;
+            }
+        }
+
+        //Sort by Revenue
+        private void SelectionSortByRevenue(string[] arr, double[] arr1)
+        {
+            int highest, idx, idx1;
+            double temp;
+            string temp1;
+            for (idx = 0; idx < arr1.Length - 1; idx++)
+            {
+                highest = idx;
+                for (idx1 = idx + 1; idx1 < arr1.Length; idx1++)
+                {
+                    if (arr1[idx1] > arr1[highest])
+                    {
+                        highest = idx1;
+                    }
+                }
+                //SelectionSort Revenue
+                temp = arr1[highest];
+                arr1[highest] = arr1[idx];
+                arr1[idx] = temp;
+
+                //Change position Movie
+                temp1 = arr[highest];
+                arr[highest] = arr[idx];
+                arr[idx] = temp1;
+            }
+        }
+
+        // Search button
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            //Open Files and create arrays
+            StreamReader inFile;
+            inFile = File.OpenText("MovieRevenue.txt");
+            int arraySize = File.ReadLines("MovieRevenue.txt").Count();
+            inFile.Close();
+            string[] movie = new string[arraySize];
+            double[] revenueSorted = new double[arraySize];
+
+            searchMovies(movie, revenueSorted);
+            revenueSearch(movie, revenueSorted);            
+        }
+
+        //Search by movies name
+        private void searchMovies(string []arr,double[]arr1)
+        {
+            //List movies when search is empty
+            if (nameRbtn.Checked && searchTb.Text == "")
+            {
+                //Read File and write in arrays
+                ReadIntoArray(arr, arr1);
+
+                //Selection Sort
+                SelectionSortByName(arr, arr1);
+
+                //Display results on listbox
+                resultsLb.Items.Clear();
+                for (int idx = 0; idx < arr.Length; idx++)
+                {
+                    DisplayRevenue(arr, arr1, idx, " >===> ");
+                }
+            }
+
+            //Search movies by name
+            if (nameRbtn.Checked && searchTb.Text != "")
+            {
+                string search = searchTb.Text.Trim();
+
+                //Read File and write in arrays
+                ReadIntoArray(arr, arr1);
+
+                //Selection Sort
+                SelectionSortByName(arr, arr1);
+
+                //Verify name inside array - Name
+                if (containsName(arr, arr1, search))
+                    containsName(arr, arr1, search);
+            }
+        }
+
+        //Verify text inside movie's name
+        private bool containsName(string[] arr, double[] arr1, string compare)
+        {
+            bool validation = false;
+            resultsLb.Items.Clear();
+            for (int idx = 0; idx < arr.Length; idx++)
+            {
+                if (arr[idx].IndexOf(compare, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    DisplayRevenue(arr, arr1, idx, " >===> ");
+
+                    validation = true;
+                }
+            }
+            if (!(validation))
+                resultsLb.Items.Add("No movie titles with those characters");
+
+            return validation;
+        }
+
+        //Search by revenue
+        private void revenueSearch(string []arr,double[]arr1)
+
+        {
+
+            if (revenueRbtn.Checked && searchTb.Text == "")
+            {
+                resultsLb.Items.Clear();
+                MessageBox.Show("Not numeric - please re-enter");
+            }
+
+            if (revenueRbtn.Checked && searchTb.Text != "")
+            {
+                //Read File and write in arrays
+                ReadIntoArray(arr, arr1);
+
+                //Selection Sort
+                SelectionSortByRevenue(arr, arr1);
+
+                //Evaluate if it is number inserted in search
+                if (double.TryParse(searchTb.Text.Trim(), out double search))
+                {
+                    BinSrch(arr, arr1, search);
+                    int found = BinSrch(arr, arr1, search);
+                    resultsLb.Items.Clear();
+                    if (search == arr1[found])
+                    {
+                        for (int idx = found; idx < arr.Length; idx++)
+                        {
+                            if (idx == found)
+                            {
+                                DisplayRevenue(arr, arr1, idx, " <===< ", " ***** EXACT");
+                            }
+                            else
+                            {
+                                DisplayRevenue(arr, arr1, idx, " <===< ");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int linearSearch = LinearSrch(arr, arr1, search);
+                        resultsLb.Items.Add("No Movie has this EXACT revenue");
+                        resultsLb.Items.Add("===============================");
+
+                        for (int idx = linearSearch; idx < arr.Length; idx++)
+                        {
+                            if (idx == linearSearch)
+                            {
+                                DisplayRevenue(arr, arr1, idx, " <===< ", " (**closest**)");
+                            }
+                            else
+                            {
+                                DisplayRevenue(arr, arr1, idx, " <===< ");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    resultsLb.Items.Clear();
+                    MessageBox.Show("Not numeric - please re-enter");
+                }
+             }
+        }
+
+        //Linear search in Revenue array
+        private int LinearSrch(string[] arr, double[] arr1, double num)
+        {
+            int index = 0;
+            for (int idx = 0; idx < arr.Length; idx++)
+                if (arr1[idx] > num)
+                    index = idx + 1;
+            
+            return index;
+        }
+
+        //Binary search in revenue array
+        private int BinSrch(string[] arr, double[]arr1, double num)
+        {
+            int first = 0;
+            int last = arr.Length - 1;
+            int mid = -1;
+
+            while (first <= last)
+            {
+                mid = (first + last) / 2;
+
+                if (arr1[mid] == num)
+                {
+                    break;
+                }
+                else
+                    if (arr1[mid] > num)
+                {
+                    first = mid + 1;
+                }
+                else
+                {
+                    last = mid - 1;
+                }
+            }
+            return mid;
+        }
+
+        //Display on listbox
+        private void DisplayRevenue(string[] arr, double[] arr1, int index, string comp1 = "", string comp2 = "")
+        {
+            resultsLb.Items.Add(arr[index] + comp1 + arr1[index].ToString("c2") + comp2);
+        }
+    }
+}
